c3468234a39cd118bb0986a984180b055a173baa
Emmanuel Macías <emmamaciasdf@gmail.com>
2024-02-13 20:21:41 -0300

    cambio de inicio flow bienvenida

--- flows/bienvenida.flow.js
+++ flows/bienvenida.flow.js
@@ -21,7 +21,8 @@ const flujoUsuariosNORegistrados = bot
 .addAnswer('Escribe *MENÚ* para mostrarte las opciones disponibles', null, null, [menuFlow])
 
 const bienvenidaFlow = bot
-    .addKeyword(bot.EVENTS.WELCOME)
+    /* .addKeyword(bot.EVENTS.WELCOME) */
+    .addKeyword('chingolo')
     .addAnswer('¡Hola👋🏼! Bienvenido a *Biogreen* 💚')
     .addAnswer('Estoy revisando si estas registrado/a ⌛...', null, async (ctx, {flowDynamic, gotoFlow, state }) => {
 
